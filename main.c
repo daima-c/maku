fe446242b3d1f4da1abf480b7ba1659070592168
daima-c <115476794+daima-c@users.noreply.github.com>
2022-10-10 22:28:00 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,52 @@
+//#pragma warning(disable:4996)
+//#define _crt_secure_no_warnings 1
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+	int num1 = 0;
+	int num2 = 0;
+	int sum = 0;
+	scanf("%d %d", &num1, &num2);
+	sum = num1 + num2;
+	printf("sum = %d\n", sum);
+	return 0;
+}
+
+
+
+
+//int main()
+//{
+//	short age = 20;
+//	float weight = 95.6f;
+//		return 0;
+//
+//}
+
+
+
+
+//int main()
+//{
+	//printf("hello world\n");
+	//printf("hello ±»Ãÿ\n");
+	//char ch = 'A';
+	//int age=20;
+	//short int age = 20;
+	//float f = 5.0;
+	//long int age = 100;
+	//double d = 3.14;
+	//printf("%d\n",sizeof(char));
+	//printf("%d\n", sizeof(short));
+	//printf("%d\n", sizeof(int));
+	//printf("%d\n", sizeof(long));
+	//printf("%d\n", sizeof(long long));
+	//printf("%d\n", sizeof(float));
+	//printf("%d\n", sizeof(double));
+	//system("pause");
+	//return 0;
+//}
